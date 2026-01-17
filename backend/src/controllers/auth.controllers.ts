72cd0e48b39b5c75dae21085cc034f0b3f5620db
MMelchiorri <mm.melchiorri@gmail.com>
2026-01-17 16:55:04 +0100

    feat: implement JWT authentication and validation for login route

--- backend/src/controllers/auth.controllers.ts
+++ backend/src/controllers/auth.controllers.ts
@@ -1,20 +1,29 @@
 import jwt from 'jsonwebtoken'
 import type { Request, Response, NextFunction } from 'express'
-
 import { getUserByEmail } from '../queries/user.query'
+import bcrypt from 'bcrypt'
 
 export async function authLogin(
   req: Request,
   res: Response,
   next: NextFunction,
 ) {
+  const { email, password } = req.body
   try {
-    const user = await getUserByEmail(req.body.email)
-    if (!user) {
+    const user = await getUserByEmail(email)
+    const isValidPassword = user
+      ? await bcrypt.compare(password, user.password || '')
+      : false
+
+    if (!isValidPassword || !user) {
       return res.status(401).json({ error: 'Invalid email or password' })
     }
-
-    res.status(201).json(user)
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET || 'secretkey',
+      { expiresIn: '1h' },
+    )
+    res.json({ token })
   } catch (error) {
     next(error)
   }
--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 
 import { authLogin } from '../controllers/auth.controllers'
+import { authSchema } from '../schemas/auth.schema'
+import { validate } from '../middlewares/validate'
 
 const router = Router()
 
-router.post('/auth/login', authLogin)
+router.post('/auth/login', validate(authSchema), authLogin)
 
 export default router
