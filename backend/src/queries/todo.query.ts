d5bf9d88cdfcbebfb28c1fc6ca59db2d2207f39e
MMelchiorri <mm.melchiorri@gmail.com>
2026-01-17 17:36:50 +0100

    feat: add post routes to main router

--- backend/src/queries/todo.query.ts
+++ backend/src/queries/todo.query.ts
@@ -1,6 +1,5 @@
 import { prisma } from '../lib/prisma'
 import type { PostModel } from '../../generated/prisma/models/Post'
-import bcrypt from 'bcrypt'
 
 export async function getPostUser(): Promise<PostModel[]> {
   return prisma.post.findMany({
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import usersRoutes from './users.routes'
 import authRoutes from './auth.routes'
+import postRoutes from './post.routes'
 import { Router } from 'express'
 
 const router = Router()
 
 router.use(usersRoutes)
 router.use(authRoutes)
+router.use(postRoutes)
 
 export default router
