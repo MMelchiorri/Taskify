c0ccace3ead9f293711a6a9b1c04ff090fe0e1ce
MMelchiorri <mm.melchiorri@gmail.com>
2026-01-28 10:16:41 +0100

    feat: add role field to user schema with default value "USER"

--- backend/src/queries/todos.query.ts
+++ backend/src/queries/todos.query.ts
@@ -1,7 +1,7 @@
 import { prisma } from '../lib/prisma'
 import type { TodoModel } from '../../generated/prisma/models/Todo'
 
-export async function getPostUser(): Promise<TodoModel[]> {
+export async function getTodosUser(): Promise<TodoModel[]> {
   return prisma.todo.findMany({
     include: { assigned: true },
   })
